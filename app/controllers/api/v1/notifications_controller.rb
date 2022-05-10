8e431df41f7d779d1980846d5b692d90a1e71dc9
Enow Mbi <benowmbi@yahoo.com>
2022-05-10 17:32:51 +0100

    Rename resouce to notifications and not announcements

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class AnnouncementsController < ApplicationController
+    class NotificationsController < ApplicationController
       def index
         @notifications = Notification.all
         render json: @notifications
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      root "announcements#index"
-      resources :announcements, only: %i[index show create update destroy]
+      root "notifications#index"
+      resources :notifications, only: %i[index show create update destroy]
     end
   end
 end
