c2c48ce6ee512833c86ef267ac178c5f6e65f041
Enow Mbi <benowmbi@yahoo.com>
2022-05-10 16:59:48 +0100

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'announcements/index'
-  get 'announcements/show'
-  get 'announcements/create'
-  get 'announcements/update'
-  get 'announcements/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :announcement, only: %i[index show create update destroy]
+    end
+  end
 end
